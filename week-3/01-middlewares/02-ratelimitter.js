55c70a52039acb66acc1e2acda6b44b86f400072
MukeshAbhi <abhimukesh284@gmail.com>
2024-09-14 23:12:46 +0530

    yet

--- week-3/01-middlewares/02-ratelimitter.js
+++ week-3/01-middlewares/02-ratelimitter.js
@@ -20,22 +20,24 @@ setInterval(() => {
 
 
 function rateLimitter ( req,res,next){
-  
-  const userId =req.headers['user-id']; 
-  
-    if(numberOfRequestsForUser[userId]){
-      numberOfRequestsForUser[userId] =  numberOfRequestsForUser[userId] + 1;
-      if(numberOfRequestsForUser[userId] > 5){
-        res.status(404).send("no entry");
-      } else {
-        next();
-      }
+
+  const userId = req.headers["user-id"];
+
+  if (numberOfRequestsForUser[userId]) {
+    numberOfRequestsForUser[userId] = numberOfRequestsForUser[userId] + 1;
+    if (numberOfRequestsForUser[userId] > 5) {
+      res.status(404).send("no entry");
     } else {
-      numberOfRequestsForUser[userId] = 1;
       next();
     }
+  } else {
+    numberOfRequestsForUser[userId] = 1;
+    next();
+  }
   
 }
+
+app.use(rateLimitter);
 
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
@@ -44,5 +46,6 @@ app.get('/user', function(req, res) {
 app.post('/user', function(req, res) {
   res.status(200).json({ msg: 'created dummy user' });
 });
+
 
 module.exports = app;
