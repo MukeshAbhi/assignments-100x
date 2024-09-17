ab5d4f763d3cdd94a636bd9aa4b92d1d29e7e7b1
MukeshAbhi <abhimukesh284@gmail.com>
2024-09-17 22:09:19 +0530

    done

--- week-3/03-mongo/middleware/user.js
+++ week-3/03-mongo/middleware/user.js
@@ -1,3 +1,5 @@
+const { User } = require("../db");
+
 async function userMiddleware(req, res, next) {
     // Implement user auth logic
     // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
@@ -5,7 +7,7 @@ async function userMiddleware(req, res, next) {
     const data = await User.findOne({username:username,password:password});
     if(!data){
         return res.status(403).json({
-            msg: "Admin doesnt exist"
+            msg: "User doesnt exist"
         })
     }else return next();
 }
--- week-3/03-mongo/routes/admin.js
+++ week-3/03-mongo/routes/admin.js
@@ -1,18 +1,41 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
-const router = Router();
+const { Admin, Course } = require("../db");
+const router = express.Router();;
 
 // Admin Routes
-router.post('/signup', (req, res) => {
-    // Implement admin signup logic
+router.post('/signup', async (req, res) => {
+     const {username, password} = req.body;
+      await Admin.create({
+        username:username,
+        password:password
+     });
+     
+     res.json({msg: "Admin created successfully"});
 });
 
-router.post('/courses', adminMiddleware, (req, res) => {
-    // Implement course creation logic
+router.post('/courses', adminMiddleware, async (req, res) => {
+    //creat a new course
+    const {title,description,price,imageLink} = req.body;
+    Course.create({
+        title: title,
+        description: description,
+        price: price,
+        imageLink: imageLink,
+        
+    });
+
+    res.json({
+        msg: 'Course created successfuly ', courseId: newCourse._id
+    })
 });
 
-router.get('/courses', adminMiddleware, (req, res) => {
+router.get('/courses', adminMiddleware, async (req, res) => {
     // Implement fetching all courses logic
+    const courses = await Course.find({});
+    res.json({
+        courses: courses
+    });
 });
 
 module.exports = router;
