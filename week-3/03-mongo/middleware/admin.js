ac096fc9cc21fa72fa0486eb4aabc38e6c2e5690
MukeshAbhi <abhimukesh284@gmail.com>
2024-09-17 21:03:30 +0530

    middled

--- week-3/03-mongo/middleware/admin.js
+++ week-3/03-mongo/middleware/admin.js
@@ -1,7 +1,18 @@
+const { Admin } = require("../db");
+
 // Middleware for handling auth
-function adminMiddleware(req, res, next) {
+async function adminMiddleware(req, res, next) {
     // Implement admin auth logic
     // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+    const {username,password } = req.headers;
+    const data = await Admin.findOne({username:username,password:password});
+    if(!data) {
+        return res.status(403).json({
+            msg: "Admin doesnt exist"
+        })
+    }else return next();
+    
+    
 }
 
 module.exports = adminMiddleware;
--- week-3/03-mongo/middleware/user.js
+++ week-3/03-mongo/middleware/user.js
@@ -1,6 +1,13 @@
-function userMiddleware(req, res, next) {
+async function userMiddleware(req, res, next) {
     // Implement user auth logic
     // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+    const {username,password } = req.headers;
+    const data = await User.findOne({username:username,password:password});
+    if(!data){
+        return res.status(403).json({
+            msg: "Admin doesnt exist"
+        })
+    }else return next();
 }
 
 module.exports = userMiddleware;
